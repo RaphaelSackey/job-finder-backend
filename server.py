a7eb0520c440a234490e4f60b5c82809c7492f4b
Raphael Sackey <raphaelsackey99@gmail.com>
2024-07-16 14:45:59 -0400

    test2 after git username chaneg anf email mistake

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import os
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, create_refresh_token
 
-#some line
+#some lin
 
 app = Flask(__name__)
 CORS(app)
